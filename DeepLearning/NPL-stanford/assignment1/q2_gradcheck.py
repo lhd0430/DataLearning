9684f8b1f2547c27ccd8c286851dc8e04b76edb8
lhd0430 <lhd0430@hotmail.com>
2018-08-23 22:21:55 -0400

    clone

--- DeepLearning/NPL-stanford/assignment1/q2_gradcheck.py
+++ DeepLearning/NPL-stanford/assignment1/q2_gradcheck.py
@@ -18,12 +18,14 @@ def gradcheck_naive(f, x):
     random.setstate(rndstate)
     fx, grad = f(x) # Evaluate function value at original point
     h = 1e-4        # Do not change this!
-
+    print(fx)
+    print(grad)
     # Iterate over all indexes ix in x to check the gradient.
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    print(it)
     while not it.finished:
         ix = it.multi_index
-
+        print(ix)
         # Try modifying x[ix] with h defined above to compute numerical
         # gradients (numgrad).
 
@@ -37,21 +39,29 @@ def gradcheck_naive(f, x):
         # to test cost functions with built in randomness later.
 
         ### YOUR CODE HERE:
-        raise NotImplementedError
+        xt = x[ix]
+        x[ix] = xt-h
+        random.setstate(rndstate)
+        f1,g1 = f(x)
+        x[ix] = xt+h
+        random.setstate(rndstate)
+        f2,g2 = f(x)
+
+        numgrad = (f2-f1)/(2*h)
+        x[ix]=xt
         ### END YOUR CODE
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
         if reldiff > 1e-5:
-            print "Gradient check failed."
-            print "First gradient error found at index %s" % str(ix)
-            print "Your gradient: %f \t Numerical gradient: %f" % (
-                grad[ix], numgrad)
+            print("Gradient check failed.")
+            print("First gradient error found at index %s" % str(ix))
+            print("Your gradient: %f \t Numerical gradient: %f" % (
+                grad[ix], numgrad))
             return
 
         it.iternext() # Step to next dimension
 
-    print "Gradient check passed!"
 
 
 def sanity_check():
@@ -60,26 +70,14 @@ def sanity_check():
     """
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
     gradcheck_naive(quad, np.array(123.456))      # scalar test
     gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
     gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
-    print ""
 
 
-def your_sanity_checks():
-    """
-    Use this space add any additional sanity checks by running:
-        python q2_gradcheck.py
-    This function will not be called by the autograder, nor will
-    your additional tests be graded.
-    """
-    print "Running your sanity checks..."
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
+
 
 
 if __name__ == "__main__":
     sanity_check()
-    your_sanity_checks()
+
